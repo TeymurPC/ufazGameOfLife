4a6f28c7f1fea4588eb9545a998161fc848c5d79
Teymur Hasanzade <aghateymur.hasanzada@ufaz.az>
2020-04-26 17:49:34 +0400

    finished main.c

--- main.c
+++ main.c
@@ -5,6 +5,15 @@
 
 int main(int argc, char const *argv[])
 {
+    struct board brd;
+    new_random_board(&brd);
+    print_board(brd);
+
+    while(1){
+        sleep(1);
+        brd = new_board(brd);
+        print_board(brd);
+    }
 
     return 0;
 }
