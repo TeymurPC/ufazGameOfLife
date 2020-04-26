5acb22c8e0f73f7d170dfd8d8ff7eb225dc8831b
GulzarJS <64319067+GulzarJS@users.noreply.github.com>
2020-04-26 17:23:54 +0400

    neighbours() function counts all alive neighbours

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "game.h"
+#include "print_to_console.h"
 
 int main(int argc, char const *argv[])
 {
     struct board brd;
     new_random_board(&brd);
+
+    // int nb_neighbours = neighbours(brd, 3,3);
+    // printf("%d\n",nb_neighbours);
 
     print_board(brd);
     
