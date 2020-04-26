ccd2288fa8987d3d6c2902eb680d017719fc5058
Teymur Hasanzade <aghateymur.hasanzada@ufaz.az>
2020-04-26 17:18:17 +0400

    fix print function to overwrite the previous lines

--- game.c
+++ game.c
@@ -22,26 +22,26 @@ void new_random_board(struct board *brd){
     }
 }
 
-struct board new_board(struct board brd){
-    struct board new_board;
-    for (int i = 0; i< BOARD_HEIGHT; i++){
-        for (int j = 0; j < BOARD_WIDTH; j++){
-            int nghbrs = neighbours(brd, j, i);
-            if (brd.cells[i][j] == ALIVE_CELL){
-                if (nghbrs == 2 || nghbrs == 3){
-                    new_board.cells[i][j] = ALIVE_CELL;
-                }else{
-                    new_board.cells[i][j] = DEAD_CELL;
-                }
-            }else{
-                if (nghbrs == 3){
-                    new_board.cells[i][j] = ALIVE_CELL;
-                }else{
-                    new_board.cells[i][j] = DEAD_CELL;
-                }
-            }
-        }
-    }
+// struct board new_board(struct board brd){
+//     struct board new_board;
+//     for (int i = 0; i< BOARD_HEIGHT; i++){
+//         for (int j = 0; j < BOARD_WIDTH; j++){
+//             int nghbrs = neighbours(brd, j, i);
+//             if (brd.cells[i][j] == ALIVE_CELL){
+//                 if (nghbrs == 2 || nghbrs == 3){
+//                     new_board.cells[i][j] = ALIVE_CELL;
+//                 }else{
+//                     new_board.cells[i][j] = DEAD_CELL;
+//                 }
+//             }else{
+//                 if (nghbrs == 3){
+//                     new_board.cells[i][j] = ALIVE_CELL;
+//                 }else{
+//                     new_board.cells[i][j] = DEAD_CELL;
+//                 }
+//             }
+//         }
+//     }
 
-    return new_board;
-}
+//     return new_board;
+// }
--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include "game.h"
+#include "print_to_console.h"
 
 int main(int argc, char const *argv[])
 {
     struct board brd;
-    new_random_board(&brd);
+    for (int i = 0; i < 50; i++)
+    {
+            
+        new_random_board(&brd);
 
-    print_board(brd);
+        print_board(brd);
+        sleep(1);
+    }
+    
     
     return 0;
 }
