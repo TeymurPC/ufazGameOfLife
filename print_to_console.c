0e75c17a4e47f49deeafc65a873cb5b9694e6e44
Teymur Hasanzade <aghateymur.hasanzada@ufaz.az>
2020-04-30 12:49:35 +0400

    Fix for neighbours faulty condition

--- print_to_console.c
+++ print_to_console.c
@@ -5,15 +5,16 @@ void print_board(struct board brd){
 
     printf("\e[2J");
     printf("\e[0;0H");
+    
     for (int i = 0; i < BOARD_HEIGHT; i++)
     {
         for (int j = 0; j < BOARD_WIDTH; j++)
         {
-            printf((brd.cells[i][j] == ALIVE_CELL ? "\033[07m  \033[m" : "  "));
+            printf((brd.cells[i][j] == ALIVE_CELL ? "\033[07m  \e[0m" : "  "));
         }
         printf("\n");
         
     }
-    // printf("\e[50A\e[50D");
-    
+
+        
 }
